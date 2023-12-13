4a1fce4de6c2cdeeec05eb5eb3e4d4c7d59562fa
Jean Bucad <jeanbucad@gmail.com>
2023-12-13 00:00:25 -0800

    remove google api key

--- src/pages/Property.jsx
+++ src/pages/Property.jsx
@@ -161,7 +161,9 @@ const Property = () => {
                                     </div>
                                 </div>
                                 <div className="mx-auto flex items-center justify-center mt-10 mb-10" style={{ width: '100%', height: '400px' }}>
-                                    <iframe src={`https://www.google.com/maps/embed/v1/place?key=AIzaSyBd2KgKk6zk7gwUnKqw1s9NJgtZ5pu3rX8&q=${streetNumber}%2C+${street}%2C+${address_state}+${suburb}+${postcode}+${country}&zoom=14`} frameBorder="0"></iframe>
+                                    <iframe width="100%"
+                                        height="100%"
+                                        allowFullScreen src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d10464.769776254596!2d153.10204664491542!3d-27.60897748948009!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6b91437fb566385b%3A0xc798107977a9f7c4!2s10c%2F11%20Kingston%20Rd%2C%20Underwood%20QLD%204119%2C%20Australia!5e0!3m2!1sen!2sus!4v1701497542634!5m2!1sen!2sus" frameBorder="0"></iframe>
                                 </div>
                             </div>
                         </div>
