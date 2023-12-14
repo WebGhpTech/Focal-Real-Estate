dbb9d00d5b9f96d191bf3b72976a6360d0cce714
Jean Bucad <jeanbucad@gmail.com>
2023-12-14 13:00:43 -0800

    update max price

--- src/components/PropertyListing.jsx
+++ src/components/PropertyListing.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 const FilterBox = ({ onClose }) => {
-  const [priceRange, setPriceRange] = useState([0, 100000]);
+  const [priceRange, setPriceRange] = useState([0, 1000]);
   const [bedroomRange, setBedroomRange] = useState([0, 25]);
   const [bathroomRange, setBathroomRange] = useState([0, 25]);
   const [carportRange, setCarportRange] = useState([0, 5]);
